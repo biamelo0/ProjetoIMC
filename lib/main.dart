7ec4c5a2d0e13729d64fd6c4a31e08a0f0a18453
biamelo0 <beatrizmelo0952@gmail.com>
2024-03-28 11:21:07 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -24,9 +26,48 @@ class Home extends StatefulWidget {
 }
 
 class _HomePageState extends State<Home> {
-  TextEditingController pesoController = new TextEditingController();
-  TextEditingController alturaController = new TextEditingController();
+  TextEditingController usuarioController = new TextEditingController();
+  TextEditingController senhaController = new TextEditingController();
   String _textoInfo = "Informe seus dados";
+
+  void _limpar_Tela(){
+    usuarioController.text = "";
+    senhaController.text = "";
+    setState(() {
+      _textoInfo = "informe seus dados";
+    });
+  }
+  void _calcular_Imc(){
+    setState(() {
+
+      double peso = double.parse(pesoController.text);
+      double altura = double.parse(alturaController.text);
+      double imc = peso / pow((altura/100), 2);
+
+      if(imc <16.5){
+        _textoInfo = "Desnutrido(${imc})";
+      }
+      else if(imc<=18.5){
+        _textoInfo = "Abaixo do peso(${imc})";
+      }
+      else if(imc<=24.9){
+        _textoInfo = "Peso ideal(${imc})";
+      }
+      else if(imc<=29.9){
+        _textoInfo = "Sobre peso(${imc})";
+      }
+      else if(imc<=34.9){
+        _textoInfo = "Obesidade Grau 1 (${imc})";
+      }
+      else if(imc<=39.9){
+        _textoInfo = "Obesidade Grau 2 (${imc})";
+      }
+      else {
+        _textoInfo = "Obesidade Grau 3 (${imc})";
+      }
+    });
+
+  }
 
 
   @override
@@ -37,7 +78,7 @@ class _HomePageState extends State<Home> {
         centerTitle: true,
         backgroundColor: Colors.blue,
         actions: <Widget>[
-        IconButton(onPressed: () {}, icon: Icon(Icons.refresh))
+        IconButton(onPressed: _limpar_Tela, icon: Icon(Icons.refresh))
         ],
       ),
       body: SingleChildScrollView(
@@ -50,7 +91,7 @@ class _HomePageState extends State<Home> {
         const Icon(
         Icons.person_outline,
           size: 120.0,
-          color: Colors.red,
+          color: Colors.lightBlueAccent,
         ),
         TextField(
           keyboardType: TextInputType.number,
@@ -80,7 +121,7 @@ class _HomePageState extends State<Home> {
             child: SizedBox(
               height: 50.0,
               child: ElevatedButton(
-                  onPressed: () {},
+                  onPressed: _calcular_Imc,
                   child: const Text("Calcular")
               ),
             ),
